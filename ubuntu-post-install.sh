674d2a9ac8c29038221aad51f37eb1bab0c6cc4b
Cullen Taylor <mctaylor@us.ibm.com>
2019-01-14 22:00:49 -0600

    Cleanup
    Signed-off-by: Cullen Taylor <mctaylor@us.ibm.com>

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -14,28 +14,19 @@ sudo dpkg -i "$TEMP_DEB"
 rm -f "$TEMP_DEB"
 
 # Install docker
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-
 sudo apt update
 sudo apt install -y docker-ce
 
 # Install kubectl
-sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+sudo apt update
+sudo apt install -y kubectl
 
 # Install ibmcloud
 curl -sL https://ibm.biz/idt-installer | bash
